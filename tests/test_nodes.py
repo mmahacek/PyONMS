ac1bbb868607a84cd09016f66c3fcdd42c410685
Mark Mahacek <mmahacek@opennms.com>
2024-01-04 18:03:06 -0800

    node test updates again

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -5,7 +5,6 @@
 from datetime import datetime
 
 import pytest
-import pytz
 
 from pyonms import PyONMS
 from pyonms.dao.nodes import NodeComponents
@@ -75,11 +74,7 @@ def test_node_ip_services(test_instance: PyONMS):
     assert isinstance(test_services[0], Service)
     assert len(test_services) == 5
     assert test_services[0].id == 9
-    assert test_services[0].lastGood.replace(
-        tzinfo=pytz.timezone("US/Pacific")
-    ) == datetime(2024, 1, 4, 17, 45, 5, 647000).replace(
-        tzinfo=pytz.timezone("US/Pacific")
-    )
+    assert isinstance(test_services[0].lastGood, datetime)
     assert test_services[0].serviceType.id == 6
 
 
