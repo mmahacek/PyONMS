5de0ccda9b5993b737f62a404b111594fe9d5a87
Mark Mahacek <mmahacek@opennms.com>
2024-01-09 20:06:50 -0800

    enlinkd dao processing all link/element types

--- pyonms/dao/enlinkd.py
+++ pyonms/dao/enlinkd.py
@@ -43,16 +43,18 @@ class EnlinkdAPI(Endpoint):
 
     def _process_topology(self, data: dict) -> pyonms.models.enlinkd.Topology:
         topology = pyonms.models.enlinkd.Topology()
+        if data.get("bridgeLinkNodes"):
+            topology.bridge_links = self._process_bridge_links(
+                data=data["bridgeLinkNodes"]
+            )
+        if data.get("cdpLinkNodes"):
+            topology.cdp_links = self._process_cdp_links(data=data["cdpLinkNodes"])
+        if data.get("isisLinkNodes"):
+            topology.isis_links = self._process_isis_links(data=data["isisLinkNodes"])
         if data.get("lldpLinkNodes"):
             topology.lldp_links = self._process_lldp_links(data=data["lldpLinkNodes"])
-        if data.get("lldpElementNode"):
-            topology.lldp_elements.append(
-                self._process_lldp_element(data=data["lldpElementNode"])
-            )
-        if data.get("isisElementNode"):
-            topology.isis_elements.append(
-                self._process_isis_element(data=data["isisElementNode"])
-            )
+        if data.get("ospfLinkNodes"):
+            topology.ospf_links = self._process_ospf_links(data=data["ospfLinkNodes"])
         if data.get("bridgeElementNodes"):
             topology.bridge_elements.append(
                 self._process_bridge_elements(data=data["bridgeElementNodes"])
@@ -61,11 +63,43 @@ class EnlinkdAPI(Endpoint):
             topology.cdp_elements.append(
                 self._process_cdp_element(data=data["cdpElementNode"])
             )
+        if data.get("isisElementNode"):
+            topology.isis_elements.append(
+                self._process_isis_element(data=data["isisElementNode"])
+            )
+        if data.get("lldpElementNode"):
+            topology.lldp_elements.append(
+                self._process_lldp_element(data=data["lldpElementNode"])
+            )
         if data.get("ospfElementNode"):
             topology.ospf_elements.append(
                 self._process_ospf_element(data=data["ospfElementNode"])
             )
         return topology
+
+    def _process_bridge_links(
+        self, data: list[dict]
+    ) -> List[pyonms.models.enlinkd.BridgeLink]:
+        links = []
+        for link in data:
+            links.append(pyonms.models.enlinkd.BridgeLink(**link))
+        return links
+
+    def _process_cdp_links(
+        self, data: list[dict]
+    ) -> List[pyonms.models.enlinkd.CdpLink]:
+        links = []
+        for link in data:
+            links.append(pyonms.models.enlinkd.CdpLink(**link))
+        return links
+
+    def _process_isis_links(
+        self, data: list[dict]
+    ) -> List[pyonms.models.enlinkd.IsIsLink]:
+        links = []
+        for link in data:
+            links.append(pyonms.models.enlinkd.IsIsLink(**link))
+        return links
 
     def _process_lldp_links(
         self, data: list[dict]
@@ -75,10 +109,21 @@ class EnlinkdAPI(Endpoint):
             links.append(pyonms.models.enlinkd.LldpLink(**link))
         return links
 
+    def _process_ospf_links(
+        self, data: list[dict]
+    ) -> List[pyonms.models.enlinkd.OspfLink]:
+        links = []
+        for link in data:
+            links.append(pyonms.models.enlinkd.OspfLink(**link))
+        return links
+
     def _process_bridge_elements(
         self, data: dict
-    ) -> pyonms.models.enlinkd.BridgeElement:
-        return pyonms.models.enlinkd.BridgeElement(**data)
+    ) -> List[pyonms.models.enlinkd.BridgeElement]:
+        elements = []
+        for element in data:
+            elements.append(pyonms.models.enlinkd.BridgeElement(**element))
+        return elements
 
     def _process_cdp_element(self, data: dict) -> pyonms.models.enlinkd.CdpElement:
         return pyonms.models.enlinkd.CdpElement(**data)
