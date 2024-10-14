915ba3283479056ea74e42e7e0db2c39c35e9a6a
mmahacek <mark@mahacek.net>
2024-10-13 20:18:05 -0700

    create Docker Compose template for spinning up an instance for running tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ def vcr_config():
 @pytest.fixture
 def test_instance() -> PyONMS:
     return PyONMS(
-        hostname=os.getenv("onms_host", "http://localhost:8980/opennms"),
-        username=os.getenv("onms_user", "admin"),
-        password=os.getenv("onms_pass", "admin"),
+        hostname=os.getenv("test_host", "http://localhost:8980/opennms"),
+        username=os.getenv("test_user", "admin"),
+        password=os.getenv("test_pass", "admin"),
     )
